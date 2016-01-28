1dd00f8d90da9c53724adf568ba3f926d70c4f8a
Frank Carey <frank@devinci.io>
2016-01-28 16:36:15 -0500

    Update the name to the correct module.

--- dkan-module-init.sh
+++ dkan-module-init.sh
@@ -1,7 +1,6 @@
-set -e
 
 # Name of the current module.
-DKAN_MODULE="dkan_workflow"
+DKAN_MODULE="visualization_entity"
 
 # DKAN branch to use
 DKAN_BRANCH="dev-dkan-ahoy-smarter"
@@ -15,11 +14,24 @@ if [[ "$PATH" != *"$COMPOSER_PATH"* ]]; then
   export PATH="$PATH:~/$COMPOSER_PATH"
 fi
 
-wget -q -O - https://raw.githubusercontent.com/NuCivic/dkan/dev-dkan-ahoy-smarter/dkan-init.sh | bash /dev/stdin $DKAN_MODULE $@ --skip-reinstall
+wget -O /tmp/dkan-init.sh https://raw.githubusercontent.com/NuCivic/dkan/$DKAN_BRANCH/dkan-init.sh
+
+# Make sure the download was at least successful.
+if [ $? -ne 0] ; then
+  echo ""
+  echo "[Error] Failed to download the dkan-init.sh script from github dkan. Branch: $DKAN_BRANCH . Perhaps someone deleted the branch?"
+  echo ""
+  exit 1
+fi
+
+#Only stop on errors starting now..
+set -e
+# OK, run the script.
+bash /tmp/dkan-init.sh $DKAN_MODULE $@ --skip-reinstall
 ahoy dkan module-link $DKAN_MODULE
 ahoy dkan module-make $DKAN_MODULE
 ahoy dkan reinstall
 ahoy drush en $DKAN_MODULE -y
 
-#Fix for behat bug not recognizing symlinked feature files or files outside it's root. See https://jira.govdelivery.com/browse/CIVIC-1005
-cp dkan_workflow/test/features/dkan_workflow.feature dkan/test/features/.
+ #Fix for behat bug not recognizing symlinked feature files or files outside it's root. See https://jira.govdelivery.com/browse/CIVIC-1005
+#cp dkan_workflow/test/features/dkan_workflow.feature dkan/test/features/.
