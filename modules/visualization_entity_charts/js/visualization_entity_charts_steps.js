6ea9bded943f963f3e4bb517ba92889b1b0fafbb
Sol Villar <msol.villar@gmail.com>
2017-05-03 18:35:03 -0300

    Refactored code for error handling

--- modules/visualization_entity_charts/js/visualization_entity_charts_steps.js
+++ modules/visualization_entity_charts/js/visualization_entity_charts_steps.js
@@ -61,10 +61,11 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
               '</div>' +
               '<div class="col-md-12" id="controls">' +
                 '<button type="button" id="prev" class="btn btn-default pull-left">Back</button>' +
-                '<button type="submit" class="form-submit btn btn-success pull-right">Finish</button>' +
+                '<button type="submit" id="finish" class="form-submit btn btn-success pull-right">Finish</button>' +
               '</div>',
     events: {
-      '#query-editor button': 'onEditorUpdate'
+      '#query-editor button': 'onEditorUpdate',
+      'click #finish': 'finish'
     },
     initialize: function(options){
       var self = this;
@@ -78,7 +79,6 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
       };
     },
     copyQueryState: function(){
-      console.log('copyQueryState');
       var self = this;
       self.state.set('queryState', self.state.get('model').queryState.toJSON());
     },
@@ -160,11 +160,22 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
       return false;
     },
     updateState: function(state, cb){
+      // TO CHECK: This never gets executed.
       cb(state);
     },
     assign: function(view, selector){
       var self = this;
       view.setElement(self.$(selector)).render();
+    },
+    finish: function(event) {
+      var self = this;
+      if (!self.validate()) {
+        event.preventDefault();
+      }
+    },
+    validate: function() {
+      var self = this;
+      return self.controls.validate();
     },
   });
 
@@ -233,8 +244,12 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
       var self = this;
       var type = self.getSelected();
       state.set('graphType', type);
-      cb(state);
-    }
+      var result = self.validate();
+      cb(state, result);
+    },
+    validate: function() {
+      return true;
+    },
   });
 
   /**
@@ -304,8 +319,12 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
       state.set('seriesFields', self.$('#control-chart-series').val());
       state.set('xfield', self.$('#control-chart-xfield').val());
       state.set('xDataType', self.$('input[name=control-chart-x-data-type]:checked').val());
-      cb(state);
-    }
+      var result = self.validate();
+      cb(state, result);
+    },
+    validate: function() {
+      return true;
+    },
   });
 
   /**
@@ -425,11 +444,13 @@ this.recline.View.nvd3 = this.recline.View.nvd3 || {};
     },
     updateStep: function(n){
       var self = this;
-      return function(state){
-        self.state = state;
-        self.gotoStep(n);
-        self.trigger('multistep:change', {step:n});
-        self.$('.chosen-choices .search-field input, .chosen-search input').attr('aria-label', 'Choose some options');
+      return function(state, success = true){
+        if (success) {
+          self.state = state;
+          self.gotoStep(n);
+          self.trigger('multistep:change', {step:n});
+          self.$('.chosen-choices .search-field input, .chosen-search input').attr('aria-label', 'Choose some options');
+        } 
       };
     },
     gotoStep: function(n){
